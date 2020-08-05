994c94c600153eeab3800dcb854dfd78dc2f7b81
daniel.soifer <danisoifer@gmail.com>
2020-08-05 11:42:46 +0300

    rebuild

---
+++ dist/precache-manifest.e53b7d155aa331a095002fddb21e6115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fdfc3f04289817b440868296b6dcfb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c99758232852f2bd92ec",
+    "url": "/static/css/main.2fe37270.chunk.css"
+  },
+  {
+    "revision": "ae49913b117cc551cdc7",
+    "url": "/static/js/2.1c3c752c.chunk.js"
+  },
+  {
+    "revision": "0445d5183d0141ac77c6557b72598011",
+    "url": "/static/js/2.1c3c752c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99758232852f2bd92ec",
+    "url": "/static/js/main.e6bcede4.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5b212e4bd96e410404bb49f982afa96a",
+    "url": "/static/media/footer-background.5b212e4b.png"
+  }
+]);
